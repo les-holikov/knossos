c6bc6ab5ddecdf23885326fdbb94b2eefa70a091
Les Holikov <sanyagolikov97@gmail.com>
2026-09-02 08:55:29 +0000

    fix(db): enable strict SSL connection for AWS RDS PostgreSQL

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
+  private readonly sslCertPath = path.join(__dirname, '..', 'certs', 'global-bundle.pem');
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
@@ -15,7 +18,10 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       password: this.configService.get<string>('db.postgres.password'),
       database: this.configService.get<string>('db.postgres.database'),
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
-    };
+      synchronize: false,
+      ssl: {
+        ca: fs.readFileSync(this.sslCertPath).toString(),
+        rejectUnauthorized: true,
+      },};
   }
 }
